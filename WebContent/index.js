a0c8c0546c5584c673b544641dbd606e48f83ce3
Darren Huynh <dfhuynh@uci.edu>
2024-04-22 19:55:49 -0700

    Added template files and changed some names

--- WebContent/index.js
+++ WebContent/index.js
@@ -1,87 +1,74 @@
-function handleMovieListResult(resultData) {
-    console.log("handleMovieListResult: populating star table from resultData");
+let cart = $("#cart");
 
+/**
+ * Handle the data returned by IndexServlet
+ * @param resultDataString jsonObject, consists of session info
+ */
+function handleSessionData(resultDataString) {
+    let resultDataJson = JSON.parse(resultDataString);
 
-    // Populate the star table
-    // Find the empty table body by id "star_table_body"
-    let starTableBodyElement = jQuery("#movie_list_table_body");
+    console.log("handle session response");
+    console.log(resultDataJson);
+    console.log(resultDataJson["sessionID"]);
 
+    // show the session information 
+    $("#sessionID").text("Session ID: " + resultDataJson["sessionID"]);
+    $("#lastAccessTime").text("Last access time: " + resultDataJson["lastAccessTime"]);
 
-    // Iterate through resultData, no more than 10 entries
-    for (let i = 0; i < Math.min(20, resultData.length); i++) {
-
-
-        // Concatenate the html tags with resultData jsonObject
-        let rowHTML = "";
-        rowHTML += "<tr>";
-        //title
-        rowHTML += "<th>" +
-            '<a href="single-movie.html?id=' + resultData[i]['movie_id'] + '">'
-            + resultData[i]["movie_title"] +
-            '</a>' + "</th>";
-        //year
-        rowHTML += "<th>" + resultData[i]["movie_year"] + "</th>";
-        //director
-        rowHTML += "<th>" + resultData[i]["movie_director"] + "</th>";
-        //genres (can be a combined string or combine them here depending)
-
-        for (let j = 1; j <= 3; j++) {
-            if (resultData[i]["movie_genre_" + j] == null) {
-                rowHTML += "<th>" + "N/A" + "</th>";
-            }
-            else {
-                rowHTML += "<th>" + resultData[i]["movie_genre_" + j] + "</th>";
-            }
-        }
-        // rowHTML += "<th>" + resultData[i]["movie_genre_1"] + "</th>";
-        // rowHTML += "<th>" + resultData[i]["movie_genre_2"] + "</th>";
-        // rowHTML += "<th>" + resultData[i]["movie_genre_3"] + "</th>";
-        //stars (same as with genres)
-        for (let j = 1; j <= 3; j++) {
-            if (resultData[i]["movie_star_" + j] == null) {
-                rowHTML += "<th>" + "N/A" + "</th>";
-            }
-            else {
-                rowHTML += "<th>" +
-                    '<a href="single-star.html?id=' + resultData[i]["movie_star_id_" + j] + '">'
-                    + resultData[i]["movie_star_" + j] +
-                    '</a>' + "</th>";
-            }
-        }
-        /*
-        rowHTML += "<th>" +
-            '<a href="single-star.html?id=' + resultData[i]['movie_star_1'] + '">'
-            + resultData[i]["movie_star_id_1"] +
-            '</a>' + "</th>";
-        rowHTML += "<th>" +
-            '<a href="single-star.html?id=' + resultData[i]['movie_star_2'] + '">'
-            + resultData[i]["movie_star_id_2"] +
-            '</a>' + "</th>";
-        rowHTML += "<th>" +
-            '<a href="single-star.html?id=' + resultData[i]['movie_star_3'] + '">'
-            + resultData[i]["movie_star_id_3"] +
-            '</a>' + "</th>";
-        */
-        //rating
-        rowHTML += "<th>" + resultData[i]["movie_rating"] + "</th>";
-        rowHTML += "</tr>";
-        // Append the row created to the table body, which will refresh the page
-        starTableBodyElement.append(rowHTML);
-    }
+    // show cart information
+    handleCartArray(resultDataJson["previousItems"]);
 }
 
+/**
+ * Handle the items in item list
+ * @param resultArray jsonObject, needs to be parsed to html
+ */
+function handleCartArray(resultArray) {
+    console.log(resultArray);
+    let item_list = $("#item_list");
+    // change it to html list
+    let res = "<ul>";
+    for (let i = 0; i < resultArray.length; i++) {
+        // each item will be in a bullet point
+        res += "<li>" + resultArray[i] + "</li>";
+    }
+    res += "</ul>";
 
+    // clear the old array and show the new array in the frontend
+    item_list.html("");
+    item_list.append(res);
+}
 
+/**
+ * Submit form content with POST method
+ * @param cartEvent
+ */
+function handleCartInfo(cartEvent) {
+    console.log("submit cart form");
+    /**
+     * When users click the submit button, the browser will not direct
+     * users to the url defined in HTML form. Instead, it will call this
+     * event handler when the event is triggered.
+     */
+    cartEvent.preventDefault();
 
-/*
-Once this .js is loaded, following scripts will be executed by the browser
-*/
+    $.ajax("api/index", {
+        method: "POST",
+        data: cart.serialize(),
+        success: resultDataString => {
+            let resultDataJson = JSON.parse(resultDataString);
+            handleCartArray(resultDataJson["previousItems"]);
+        }
+    });
 
+    // clear input form
+    cart[0].reset();
+}
 
-// Makes the HTTP GET request and registers on success callback function handleStarResult
-jQuery.ajax({
-    dataType: "json", // Setting return data type
-    method: "GET", // Setting request method
-    url: "api/movieList", // Setting request url, which is mapped by StarsServlet in Stars.java
-    success: (resultData) => handleMovieListResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
+$.ajax("api/index", {
+    method: "GET",
+    success: handleSessionData
 });
+
+// Bind the submit action of the form to a event handler function
+cart.submit(handleCartInfo);
---
+++ WebContent/movie-list.js
@@ -0,0 +1,87 @@
+function handleMovieListResult(resultData) {
+    console.log("handleMovieListResult: populating star table from resultData");
+
+
+    // Populate the star table
+    // Find the empty table body by id "star_table_body"
+    let starTableBodyElement = jQuery("#movie_list_table_body");
+
+
+    // Iterate through resultData, no more than 10 entries
+    for (let i = 0; i < Math.min(20, resultData.length); i++) {
+
+
+        // Concatenate the html tags with resultData jsonObject
+        let rowHTML = "";
+        rowHTML += "<tr>";
+        //title
+        rowHTML += "<th>" +
+            '<a href="single-movie.html?id=' + resultData[i]['movie_id'] + '">'
+            + resultData[i]["movie_title"] +
+            '</a>' + "</th>";
+        //year
+        rowHTML += "<th>" + resultData[i]["movie_year"] + "</th>";
+        //director
+        rowHTML += "<th>" + resultData[i]["movie_director"] + "</th>";
+        //genres (can be a combined string or combine them here depending)
+
+        for (let j = 1; j <= 3; j++) {
+            if (resultData[i]["movie_genre_" + j] == null) {
+                rowHTML += "<th>" + "N/A" + "</th>";
+            }
+            else {
+                rowHTML += "<th>" + resultData[i]["movie_genre_" + j] + "</th>";
+            }
+        }
+        // rowHTML += "<th>" + resultData[i]["movie_genre_1"] + "</th>";
+        // rowHTML += "<th>" + resultData[i]["movie_genre_2"] + "</th>";
+        // rowHTML += "<th>" + resultData[i]["movie_genre_3"] + "</th>";
+        //stars (same as with genres)
+        for (let j = 1; j <= 3; j++) {
+            if (resultData[i]["movie_star_" + j] == null) {
+                rowHTML += "<th>" + "N/A" + "</th>";
+            }
+            else {
+                rowHTML += "<th>" +
+                    '<a href="single-star.html?id=' + resultData[i]["movie_star_id_" + j] + '">'
+                    + resultData[i]["movie_star_" + j] +
+                    '</a>' + "</th>";
+            }
+        }
+        /*
+        rowHTML += "<th>" +
+            '<a href="single-star.html?id=' + resultData[i]['movie_star_1'] + '">'
+            + resultData[i]["movie_star_id_1"] +
+            '</a>' + "</th>";
+        rowHTML += "<th>" +
+            '<a href="single-star.html?id=' + resultData[i]['movie_star_2'] + '">'
+            + resultData[i]["movie_star_id_2"] +
+            '</a>' + "</th>";
+        rowHTML += "<th>" +
+            '<a href="single-star.html?id=' + resultData[i]['movie_star_3'] + '">'
+            + resultData[i]["movie_star_id_3"] +
+            '</a>' + "</th>";
+        */
+        //rating
+        rowHTML += "<th>" + resultData[i]["movie_rating"] + "</th>";
+        rowHTML += "</tr>";
+        // Append the row created to the table body, which will refresh the page
+        starTableBodyElement.append(rowHTML);
+    }
+}
+
+
+
+
+/*
+Once this .js is loaded, following scripts will be executed by the browser
+*/
+
+
+// Makes the HTTP GET request and registers on success callback function handleStarResult
+jQuery.ajax({
+    dataType: "json", // Setting return data type
+    method: "GET", // Setting request method
+    url: "api/movieList", // Setting request url, which is mapped by StarsServlet in Stars.java
+    success: (resultData) => handleMovieListResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
+});
