980b6a41d89bcf4f91975e27e1397ababeb05483
DH572 <115117969+DH572@users.noreply.github.com>
2024-04-27 22:32:05 -0700

    Update SearchResultsServlet.java

--- src/SearchResultsServlet.java
+++ src/SearchResultsServlet.java
@@ -51,13 +51,16 @@ public class SearchResultsServlet extends HttpServlet {
         String year = request.getParameter("year");
         String director = request.getParameter("director");
         String star = request.getParameter("star");
+        String movieNumber = request.getParameter("movie_number");
+        String order = request.getParameter("order");
+        String page = request.getParameter("page");
 
         // Get a connection from dataSource and let resource manager close the connection after usage.
         try (Connection conn = dataSource.getConnection()) {
             // Declare our statement
             Statement statementOne = conn.createStatement();
             // Get top 20 movies
-            String queryTopMovies = "SELECT DISTINCT movies.id, movies.title, movies.year, movies.director, ratings.rating, movies.price " +
+            String queryTopMovies = "SELECT DISTINCT movies.id, movies.title, movies.year, movies.director, ratings.rating " +
                     "FROM movies " +
                     "JOIN ratings ON movies.id = ratings.movieId " +
                     "JOIN genres_in_movies ON movies.id = genres_in_movies.movieId " +
@@ -81,12 +84,55 @@ public class SearchResultsServlet extends HttpServlet {
             if (star != null && !star.isEmpty()) {
                 queryTopMovies += " AND stars.name LIKE '%" + star + "%'";
             }
-            queryTopMovies += " ORDER BY ratings.rating DESC;";
 
+            // Implement ordering and limiting
+            String first_sort = "";
+            String first_sort_order = "";
+            String second_sort = "";
+            String second_sort_order = "";
+            //checking to see the ordering
+            if (order.charAt(0) == 't'){
+                first_sort = "movies.title";
+                second_sort = "ratings.rating";
+            } else {
+                first_sort = "ratings.rating";
+                second_sort = "movies.title";
+            }
+            if (order.charAt(1) == 'a'){
+                first_sort_order = "ASC";
+            } else {
+                first_sort_order = "DESC";
+            }
+            if (order.charAt(3) == 'a'){
+                second_sort_order = "ASC";
+            } else {
+                second_sort_order = "DESC";
+            }
+
+            //Get offset by using page number and movie number
+            String offset_value = String.valueOf(Integer.parseInt(movieNumber) * (Integer.parseInt(page)-1));
+            //this is to see if there is a next
+            String offset_value_future = String.valueOf(Integer.parseInt(movieNumber) * (Integer.parseInt(page)));
+
+            String queryCheckFuture = queryTopMovies + " ORDER BY " + first_sort + " " + first_sort_order + ", " + second_sort +
+                    " " + second_sort_order + " LIMIT " + movieNumber + " OFFSET " + offset_value_future + ";";
+
+            queryTopMovies += " ORDER BY " + first_sort + " " + first_sort_order + ", " + second_sort +
+                    " " + second_sort_order + " LIMIT " + movieNumber + " OFFSET " + offset_value + ";";
+
+            JsonObject jsonObject0 = new JsonObject();
+
+            ResultSet futureMovies = statementOne.executeQuery(queryCheckFuture);
+            if (!futureMovies.next()) {
+                jsonObject0.addProperty("future", "false");
+            } else {
+                jsonObject0.addProperty("future", "true");
+            }
 
             ResultSet topMovies = statementOne.executeQuery(queryTopMovies);
-            JsonArray jsonArray = new JsonArray();
 
+            JsonArray jsonArray = new JsonArray();
+            jsonArray.add(jsonObject0);
 
             while (topMovies.next()) {
                 // Retrieve title, year, director, and rating from top movies
@@ -94,15 +140,14 @@ public class SearchResultsServlet extends HttpServlet {
                 String movie_year = topMovies.getString("year");
                 String movie_director = topMovies.getString("director");
                 String movie_rating = topMovies.getString("rating");
-                String movie_price = topMovies.getString("price");
 
                 // Retrieve 3 genres and 3 stars max for top movies
                 String movie_id = topMovies.getString("id");
                 Statement statementTwo = conn.createStatement();
-                String queryGenres = "SELECT genres.name FROM movies JOIN genres_in_movies ON genres_in_movies.movieId = movies.id JOIN genres ON genres.id = genres_in_movies.genreId WHERE movies.id = '" + movie_id + "' LIMIT 3;";
+                String queryGenres = "SELECT genres.name FROM movies JOIN genres_in_movies ON genres_in_movies.movieId = movies.id JOIN genres ON genres.id = genres_in_movies.genreId WHERE movies.id = '" + movie_id + "' ORDER BY genres.name ASC LIMIT 3;";
                 ResultSet genres = statementTwo.executeQuery(queryGenres);
                 Statement statementThree = conn.createStatement();
-                String queryStars = "SELECT stars.name, stars.id FROM movies JOIN stars_in_movies ON stars_in_movies.movieId = movies.id JOIN stars ON stars.id = stars_in_movies.starId WHERE movies.id = '" + movie_id + "' LIMIT 3;";
+                String queryStars = "SELECT stars.name, stars.id FROM movies JOIN stars_in_movies ON stars_in_movies.movieId = movies.id JOIN stars ON stars.id = stars_in_movies.starId WHERE movies.id = '" + movie_id + "' ORDER BY stars.name ASC LIMIT 3;";
                 ResultSet stars = statementThree.executeQuery(queryStars);
 
 
@@ -113,7 +158,6 @@ public class SearchResultsServlet extends HttpServlet {
                 jsonObject.addProperty("movie_director", movie_director);
                 jsonObject.addProperty("movie_rating", movie_rating);
                 jsonObject.addProperty("movie_id", movie_id);
-                jsonObject.addProperty("movie_price", movie_price);
 
 
                 int genre_count = 1;
