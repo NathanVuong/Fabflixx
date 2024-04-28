8b906751c471b0bb9fc86e4bad21a7cd8177a3e9
DH572 <115117969+DH572@users.noreply.github.com>
2024-04-27 23:03:36 -0700

    Update search-result.js

--- WebContent/search-result.js
+++ WebContent/search-result.js
@@ -101,6 +101,7 @@ function handleSearchResult(resultData) {
         }
         //rating
         rowHTML += "<th>" + resultData[i]["movie_rating"] + "</th>";
+        rowHTML += "<th><button onclick=\"addItem('" + resultData[i]["movie_title"] + "', " + resultData[i]["movie_price"] + ")\">Add</button></th>";
         rowHTML += "</tr>";
         // Append the row created to the table body, which will refresh the page
         resultTableBodyElement.append(rowHTML);
